2bb4eaad69a4c36723369cabb1bf96ab86a88eaf
HasnatSN <hasnat.kontakt@gmail.com>
2022-04-19 21:56:31 +0200

    first exercise

---
+++ exercise_one.js
@@ -0,0 +1,14 @@
+// Return the sum of all the the numbers smaller than the input that are either devideable by 3 or 5
+
+function solution(number) {
+  var sum = 0;
+
+  for (var i = 1; i < number; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+
